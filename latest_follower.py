b13bafe85ec3ee291004203b5e84ddab271b0eda
Piotr Patek <piotr.patek.stud@pw.edu.pl>
2023-10-20 09:37:17 +0200

    Latest update

--- latest_follower.py
+++ latest_follower.py
@@ -32,6 +32,11 @@ class LineFollower:
         # Init color sensors
         self._left_color_sensor = ColorSensor(left_sensor)
         self._right_color_sensor = ColorSensor(right_sensor)
+        print("Configuration complete")
+
+    def calculate_pid_steering(self, kp: float, ki: float, kd: float):
+        pass
+
 
     def follow_line_2_sensors(self, kp, ki, kd, speed, follow_time, target_light_intensity=None, follow_left_edge=True, white=60,
                               off_line_count_max=20, sleep_time=0.01):
@@ -43,21 +48,24 @@ class LineFollower:
         speed_native_units = speed.to_native_units(self.tank.left_motor)
         start_time = time.time()
         end_time = time.time()
+        print("Start following")
+        #self._right_color_sensor.calibrate_white()
+        #self._left_color_sensor.calibrate_white()
 
-        while end_time - start_time >= follow_time:
+        while end_time - start_time <= follow_time:
             error = self._left_color_sensor.reflected_light_intensity - self._right_color_sensor.reflected_light_intensity
+            print("Left: ", self._left_color_sensor.reflected_light_intensity, "Right: ", self._right_color_sensor.reflected_light_intensity )
             integral = integral + error
             derivative = error - last_error
             last_error = error
             turn_native_units = (kp * error) + (ki * integral) + (kd * derivative)
-
-            if not follow_left_edge:
-                turn_native_units *= -1
+            print("Native units: ", turn_native_units)
 
             left_speed = SpeedNativeUnits(speed_native_units - turn_native_units)
             right_speed = SpeedNativeUnits(speed_native_units + turn_native_units)
+            print("Left speed: ", left_speed, " ", "Right speed: ", right_speed)
 
-            # Have we lost the line?
+            """# Have we lost the line?
             if self._left_color_sensor.reflected_light_intensity >= white and self._right_color_sensor.reflected_light_intensity >= white:
                 off_line_count += 1
 
@@ -65,24 +73,29 @@ class LineFollower:
                     self.tank.stop()
                     raise LineFollowErrorLostLine("we lost the line")
             else:
-                off_line_count = 0
+                off_line_count = 0"""
 
             if sleep_time:
                 time.sleep(sleep_time)
 
             try:
-                self.tank.on(left_speed, right_speed)
+                pass
+                #self.tank.on(left_speed, right_speed)
             except SpeedInvalid as e:
                 self.tank.stop()
                 raise LineFollowErrorTooFast("The robot is moving too fast to follow the line")
 
+            end_time = time.time()
+
         self.tank.stop()
+        print("Following ended")
 
 
 if __name__ == "__main__":
-    follower = LineFollower(OUTPUT_C, OUTPUT_A, INPUT_1, INPUT_2, "inversed", "inversed")
+    follower = LineFollower(OUTPUT_C, OUTPUT_A, INPUT_2, INPUT_1, "inversed", "inversed")
+    follower.follow_line_2_sensors(kp=1, ki=0.05, kd=0.05, speed=15, follow_time=4500, sleep_time=0.01)
 
-    try:
+    """try:
         # Follow the line for 4500ms
         follower.tank.follow_line(
             kp=11.3, ki=0.05, kd=3.2,
@@ -92,4 +105,4 @@ if __name__ == "__main__":
             ms=4500
         )
     except LineFollowErrorTooFast:
-        follower.tank.stop()
+        follower.tank.stop()"""
